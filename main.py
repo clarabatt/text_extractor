34322a2355eee9a33cd7f4c2347d65e89ee9c2df
Clara Battesini <clarabattesini@gmail.com>
2023-04-24 20:33:01 -0400

    feat: creating a route to upload the file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from typing import Union
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from typing import Union
+import process_image
 
 app = FastAPI()
 
@@ -11,24 +12,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 @app.get("/")
 def read_root(req: Request):
     return templates.TemplateResponse("home.html", {"request": req})
 
+@app.post("/upload")
+async def upload_file(req: Request, file: UploadFile or None = None):
+    if not file:
+        return templates.TemplateResponse("home.html", { "request": req, "text":  "No file uploaded"})
+    else:
+        text = process_image.extract(file.filename)
+        return templates.TemplateResponse("home.html", { "request": req, "text":  text})
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-@app.put("items/{item_id}")
-def update_item(item_id: int, iem: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(req: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": req, "id": id})
