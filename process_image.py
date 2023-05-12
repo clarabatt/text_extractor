15ea57e2045d7a1869104434f4578a4671aa1207
Clara Battesini <clarabattesini@gmail.com>
2023-05-12 09:38:21 -0400

    feat: implemented the extraction module

--- process_image.py
+++ process_image.py
@@ -1,3 +1,9 @@
+from io import BytesIO
+from PIL import Image
+import pytesseract
 
-def extract(filePath: str):
-    return "text"
+
+def extract(content: bytes):
+    image = Image.open(BytesIO(content))
+    text = pytesseract.image_to_string(image)
+    return text
