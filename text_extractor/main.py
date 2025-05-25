72268272f64eb1b30730de2a3c453fbeb114bcb0
Clara Battesini <clarabattesini@gmail.com>
2025-05-25 18:01:59 -0400

    chore: improve code

--- text_extractor/main.py
+++ text_extractor/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, Request, UploadFile
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from typing import Union
-import text_extractor.process_image as process_image
+from text_extractor import process_image
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 
@@ -19,11 +16,15 @@ def read_root(req: Request):
 
 
 @app.post("/upload")
-async def upload_file(req: Request, file: UploadFile or None = None):
+async def upload_file(req: Request, file: UploadFile | None = None):
     if not file:
-        return templates.TemplateResponse("home.html", {"request": req, "text":  "No file uploaded"})
-    else:
-        content = await file.read()
-        filename = file.filename
-        text = process_image.extract(content)
-        return templates.TemplateResponse("home.html", {"request": req, "text":  text, "filename": filename})
+        return templates.TemplateResponse(
+            "home.html", {"request": req, "text": "No file uploaded"}
+        )
+
+    content = await file.read()
+    filename = file.filename
+    text = process_image.extract(content)
+    return templates.TemplateResponse(
+        "home.html", {"request": req, "text": text, "filename": filename}
+    )
